29a1b158267abae72eeab79103914b533c4e6df8
4wl2d <loki.lafeyson.00@list.ru>
2023-01-11 23:21:09 +0300

    Start -> Photoshoping schedule for days

--- app/src/main/java/ind/kait/isp211/MainActivity.java
+++ app/src/main/java/ind/kait/isp211/MainActivity.java
@@ -35,13 +35,11 @@ public class MainActivity extends AppCompatActivity {
 
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
     }
 
     @Override
     public boolean onSupportNavigateUp() {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
+        return super.onSupportNavigateUp();
     }
 }
